2cf2aae929846f139cdcf3575f7ed37a9cd48a19
Stanislav Lyahnovich <stas.lyahnovich@gmail.com>
2025-10-30 01:09:33 +0300

    fixed for sdl ttf

--- sdl2/xmake.lua
+++ sdl2/xmake.lua
@@ -1,10 +1,14 @@
 add_requires("libsdl2")
-add_requires("libsdl2_ttf")
+if has_config("WIDE") or has_config("UTF8") then
+    add_requires("libsdl2_ttf")
+end
 
 target("backend")
     set_kind("static")
     add_files("*.c")
     add_deps("pdcurses")
     add_packages("libsdl2")
-    add_packages("libsdl2_ttf")
+    if has_config("WIDE") or has_config("UTF8") then
+        add_packages("libsdl2_ttf")
+    end
     add_options("DEBUG","DLL","WIDE","UTF8","INFOEX")
